f055c207030bfb80da0704793526bc3167c8d5e3
asim.khalil <asim.khalil@novatesoft.io>
2020-06-24 12:17:13 +0500

    Response

---
+++ src/main/java/com/cucumber/poc/vo/Response.java
@@ -0,0 +1,27 @@
+package com.cucumber.poc.vo;
+
+import org.springframework.http.HttpStatus;
+
+public class Response {
+
+    private HttpStatus status;
+
+    private String message;
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
